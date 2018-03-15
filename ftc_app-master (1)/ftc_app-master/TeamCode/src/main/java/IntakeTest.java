d43cd013093a6ed757f1152af9367c4337a355f0
MK6371 <6371machinekings@gmail.com>
2018-03-15 16:03:51 -0700

    Added basic FTC files
    Trevor's first commit.

---
+++ ftc_app-master (1)/ftc_app-master/TeamCode/src/main/java/IntakeTest.java
@@ -0,0 +1,36 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+/**
+ * Created by 300187 on 3/14/2018.
+ */
+@TeleOp(name = "IntakeTest")
+public class IntakeTest extends OpMode {
+    //declare your variables for Android Studio
+    public CRServo intakeLeft;
+    public CRServo intakeRight;
+
+    @Override 
+    public void init() {
+        //declare on the phone
+        intakeLeft = hardwareMap.crservo.get("intakeLeft");
+        intakeRight = hardwareMap.crservo.get("intakeRight");
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.right_bumper){
+            intakeLeft.setPower(-1);    //INTAKE
+            intakeRight.setPower(-1);
+        }
+else if(gamepad1.left_bumper){
+            intakeLeft.setPower(1);     //OUTAKE
+            intakeRight.setPower(1);
+        }
+        else{
+    intakeLeft.setPower(0);
+    intakeRight.setPower(0);
+        }
+    }
+}
